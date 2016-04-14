73006bd5e30f101e8ce3dc010d466733a026f72f
yas-mnkornym <yasuhiro@aurora.ocn.ne.jp>
2016-04-14 12:49:59 +0900

    Enabled manual versioning

---
+++ extras/AssemblyInfoCmdlet/AssemblyInfoCmdlet/AssemblyInfoAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AssemblyInfoCmdlet
+{
+	[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+	public sealed class AssemblyInfoPropertyAttribute : Attribute
+	{
+		public string PropertyName { get; set; }
+	}
+}
