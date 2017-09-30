90f0cf70a0db0f57b0e4e81ff43e06ab46dd0539
manayome <manayome@dummy.com>
2017-09-30 20:18:26 +0900

    Update ApplyNugetVersions.ps1 to skip unsupported projects.

--- scripts/ApplyNugetVersions.ps1
+++ scripts/ApplyNugetVersions.ps1
@@ -6,12 +6,18 @@ $assemblyInfoCmdletPath = Join-Path (Join-Path $ProjectRoot bin) AssemblyInfoCmd
 Import-Module $assemblyInfoCmdletPath
 
 (ls $ProjectRoot -Recurse).Where{ $_.Extension -eq ".nuspec" }.ForEach{
-  $assemblyInfoPath = Join-Path (Join-Path $_.DirectoryName Properties) AssemblyInfo.cs
-  $assemblyInfo = Get-AssemblyInfo $assemblyInfoPath
-  $tokens = $assemblyInfo.AssemblyVersion.Split(".")
-  $packageVersion = $tokens[0] + "." + $tokens[1] + "." + $tokens[2]
+	try{
+		$assemblyInfoPath = Join-Path (Join-Path $_.DirectoryName Properties) AssemblyInfo.cs
+		$assemblyInfo = Get-AssemblyInfo $assemblyInfoPath
+		$tokens = $assemblyInfo.AssemblyVersion.Split(".")
+		$packageVersion = $tokens[0] + "." + $tokens[1] + "." + $tokens[2]
 
-  [xml]$xml = Get-Content $_.FullName
-  $xml.package.metadata.version = $packageVersion + "-beta"
-  $xml.Save($_.FullName)
+		[xml]$xml = Get-Content $_.FullName
+		$xml.package.metadata.version = $packageVersion + "-beta"
+		$xml.Save($_.FullName)
+	}
+	catch [DirectoryNotFoundException]{
+		Write-Host "Failed to load Assembly.cs."
+		Write-Host "Skipping " + $assemblyInfoPath
+	}
 }
